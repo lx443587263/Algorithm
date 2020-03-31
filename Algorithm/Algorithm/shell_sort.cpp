26725a6ecb2e58b1186b780aa30d65541fa9c375
lx443587263 <43426915+lx443587263@users.noreply.github.com>
2020-03-31 18:11:38 +0800

    update template

--- Algorithm/Algorithm/shell_sort.cpp
+++ Algorithm/Algorithm/shell_sort.cpp
@@ -1,6 +1,22 @@
-#include <stdio.h>
 
-void shell_sort(int a[], int n);
+template<typename T>
+void shell_sort(T *a, const size_t n)
+{
+	T i = 0, j = 0, k = 0, temp = 0;
+	for ( k =n/ 2; k >=1; k/=2)
+	{
+		for (i = k; i <= n - 1; ++i)
+		{
+			temp = a[i];
+			for ( j = i-k; j>0&&a[j]>temp; j-=k)
+			{
+				a[j + k] = a[j];
+			}
+			a[j + k] = temp;
+		}
+	}
+}
+
 int main()
 {
 	int a[] = { 2,3,1,7,4,5,6,9,8,0 };
@@ -9,19 +25,3 @@ int main()
 }
 
 
-void shell_sort(int a[],int n)
-{
-	int i, j, k, temp;
-	for (k =n / 2; k >=1; k/=2)
-	{
-		for (i = k; i <=n-1; ++i)
-		{
-			temp = a[i];
-			for (j = i-k; temp<a[j]&&j>=0; j-=k)
-			{
-				a[j + k] = a[j];
-			}
-			a[j+k] = temp;
-		} 
-	}
-}
