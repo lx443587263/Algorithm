b1044acaeaad33385ba213c46edc22412222fa15
lx443587263 <43426915+lx443587263@users.noreply.github.com>
2020-04-23 18:10:08 +0800

    Update inser_sort.cpp

--- Algorithm/Algorithm/inser_sort.cpp
+++ Algorithm/Algorithm/inser_sort.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <iostream>
+#include<ctime>
+#include <algorithm>
+#include <vector>
 
-template<typename T>
+
+/*template<typename T>
 void insert_sort(T *a, const size_t n)
 {
 	T temp = 0, j = 0;
@@ -13,13 +18,92 @@ void insert_sort(T *a, const size_t n)
 		}
 		a[j + 1] = temp;
 	}
+}*/
+
+class A
+{
+public:
+	int _id;
+};
+
+class Gen_fn
+{
+public:
+	A operator ()(void)
+	{
+		return { rand() % 100 };
+	}
+}gen;
+
+
+class A_Comp
+{
+public:
+	bool operator()(A const& a, A const& b)
+	{
+		return a._id < b._id;
+	}
+} comp;
+
+template <typename T,typename Compare>
+void insert_sort(std::vector<T>& vec, Compare comp)
+{
+	T temp;
+	for (int i = 1; i < vec.size(); ++i)
+	{
+		temp = std::move(vec[i]);
+		int j = i - 1;
+		for (; comp(temp, vec[j]);)
+		{
+			vec[j + 1] = std::move(vec[j]);
+			if (--j < 0)
+			{
+				break;
+			}
+		}
+		vec[j+1] = std::move(temp);
+	}
+}
+
+//iterator
+template <typename T, typename Compare>
+void insert_sort1(std::vector<T>& vec, Compare comp)
+{
+	T temp;
+	for (auto it =vec.begin()+1;it!=vec.end();++it)
+	{
+		temp = std::move(*it);
+		auto it_2 = (it - 1);
+		for (; comp(temp, *it_2); --it_2)
+		{
+			*(it_2+1) = std::move(*it_2);
+			if (it_2 == vec.begin())
+				break;
+		}
+		if (it_2 == vec.begin())
+			*it_2 = std::move(temp);
+		else
+			*(it_2 + 1) = std::move(temp);
+	}
+}
+
+
+A gen_fn(void)
+{
+	return { rand() % 100 };
 }
 
 int main()
 {
-	int a[] = { 2,3,1,7,4,5,6,9,8,0 };
-	insert_sort(a, sizeof(a) / sizeof(a[0]));
+	std::vector<A> vec(10);
 
+	srand(static_cast<unsigned>(time(nullptr)));
+
+	std::generate(vec.begin(), vec.end(), gen_fn);
+	insert_sort(vec, comp);
+	for (auto& it : vec)
+	{
+		std::cout << it._id << std::endl;
+	}
 	return 0;
 }
-
